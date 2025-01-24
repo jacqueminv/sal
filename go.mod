8e9b185bf0b7330b057b5faf06f8a7f0bc6380d5
Valentin Jacquemin <jacqueminv@gmail.com>
2025-01-24 07:54:31 +0100

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jacqueminv/rbr-strava-log
+
+go 1.23.3
---
+++ rbr-strava-log.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// get required configuration from env vars
+
+	// read locally the current Strava Tokens
+
+	// fetch an access token (if necessary)
+
+	// write locally the updated Strava Tokens
+
+	// outputs the activities over the past year
+}
