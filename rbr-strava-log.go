c47c0c86a1957a46237c9926108ecaa239ee7d40
Valentin Jacquemin <jacqueminv@gmail.com>
2025-01-24 21:42:17 +0100

    Fetches Access Token if necessary

--- rbr-strava-log.go
+++ rbr-strava-log.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 type StravaAuthConfig struct {
@@ -17,6 +22,10 @@ type StravaToken struct {
 	RefreshToken string `json:"refresh_token"`
 	AccessToken  string `json:"access_token"`
 }
+
+const (
+	fiftyWeeks = time.Hour * 24 * 7 * 50
+)
 
 var (
 	authConfig StravaAuthConfig
@@ -86,6 +95,76 @@ func writeState(tokens *StravaToken) {
 	}
 }
 
+func token(form url.Values) StravaToken {
+	url := "https://www.strava.com/oauth/token"
+
+	resp, err := http.PostForm(url, form)
+	if err != nil || resp.StatusCode != 200 {
+		log.Fatal("Failed to interact with the token api")
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	var response StravaToken
+	if err := json.Unmarshal(body, &response); err != nil {
+		log.Fatal("Failed to unmarshal Strava Response")
+		panic(err)
+	}
+
+	return response
+}
+func confirmCode(code string) StravaToken {
+	form := url.Values{}
+	form.Add("client_id", authConfig.clientId)
+	form.Add("client_secret", authConfig.clientSecret)
+	form.Add("grant_type", "authorization_code")
+	form.Add("code", code)
+
+	return token(form)
+}
+func refresh(tokens *StravaToken) {
+	form := url.Values{}
+	form.Add("client_id", authConfig.clientId)
+	form.Add("client_secret", authConfig.clientSecret)
+	form.Add("grant_type", "refresh_token")
+	form.Add("refresh_token", tokens.RefreshToken)
+
+	t := token(form)
+	tokens.RefreshToken = t.RefreshToken
+	tokens.AccessToken = t.AccessToken
+	tokens.ExpiresAt = t.ExpiresAt
+}
+func accessToken(tokens *StravaToken) {
+	var now = time.Now().UTC()
+	if tokens.ExpiresAt < now.Unix() && len(tokens.RefreshToken) > 0 {
+		refresh(tokens)
+	}
+
+	if tokens.ExpiresAt < now.Unix() {
+		quit := make(chan StravaToken)
+		acceptsTokenHandler := func(w http.ResponseWriter, req *http.Request) {
+			if req.URL.Path == "/" {
+				q := req.URL.Query()
+				code := q.Get("code")
+				io.WriteString(w, "<!doctype html><html><body><p>You can close this page, the Strava Authorization proceeded successfully.</p></body>")
+				resp := confirmCode(code)
+				quit <- resp
+			}
+		}
+		go func() {
+			http.HandleFunc("/", acceptsTokenHandler)
+			log.Fatal(http.ListenAndServe(":8080", nil))
+		}()
+		t := <-quit
+		tokens.ExpiresAt = t.ExpiresAt
+		tokens.RefreshToken = t.RefreshToken
+		tokens.AccessToken = t.AccessToken
+	}
+
+}
+
 func main() {
 	clientId := os.Getenv("SL_CLIENT_ID")
 	clientSecret := os.Getenv("SL_CLIENT_SECRET")
@@ -98,9 +177,7 @@ func main() {
 
 	var tokens StravaToken
 	readState(&tokens)
-
-	// fetch an access token (if necessary)
-
+	accessToken(&tokens)
 	writeState(&tokens)
 
 	// outputs the activities over the past year
