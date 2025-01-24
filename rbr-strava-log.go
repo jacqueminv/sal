5d09c221c4b2dce86ad9505de6fe1b1cc5ca7e3d
Valentin Jacquemin <jacqueminv@gmail.com>
2025-01-24 07:56:10 +0100

    Gets auth config via environment variables

--- rbr-strava-log.go
+++ rbr-strava-log.go
@@ -1,7 +1,23 @@
 package main
 
+type StravaAuthConfig struct {
+	clientId     string
+	clientSecret string
+}
+
+var (
+	authConfig StravaAuthConfig
+)
+
 func main() {
-	// get required configuration from env vars
+	clientId := os.Getenv("SL_CLIENT_ID")
+	clientSecret := os.Getenv("SL_CLIENT_SECRET")
+	if clientId == "" || clientSecret == "" {
+		log.Fatal("Missing SL_CLIENT_ID, SL_CLIENT_SECRET")
+		os.Exit(1)
+	}
+	authConfig.clientId = clientId
+	authConfig.clientSecret = clientSecret
 
 	// read locally the current Strava Tokens
 
