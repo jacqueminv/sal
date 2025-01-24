67f309ecb2f421ed260c3b7d20e35a0917b1cac2
Valentin Jacquemin <jacqueminv@gmail.com>
2025-01-24 07:59:38 +0100

    Reads StravaToken from local disk

--- rbr-strava-log.go
+++ rbr-strava-log.go
@@ -1,13 +1,77 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"path/filepath"
+)
+
 type StravaAuthConfig struct {
 	clientId     string
 	clientSecret string
 }
 
+type StravaToken struct {
+	ExpiresAt    int64  `json:"expires_at"`
+	RefreshToken string `json:"refresh_token"`
+	AccessToken  string `json:"access_token"`
+}
+
 var (
 	authConfig StravaAuthConfig
 )
+
+func configPath() string {
+	var configPath string
+
+	dir, dirErr := os.UserConfigDir()
+	if dirErr == nil {
+		configPath = filepath.Join(dir, "rbr-strava", "state.json")
+		var err error
+		_, err = os.ReadFile(configPath)
+		if err != nil && !os.IsNotExist(err) {
+			log.Fatal(err)
+			panic(err)
+		} else if err != nil && os.IsNotExist(err) {
+			err = os.MkdirAll(filepath.Dir(configPath), 0700)
+			if err != nil {
+				log.Fatal(err)
+				panic(err)
+			}
+		}
+	} else {
+		log.Fatal("Failed to get UserConfigDir")
+		panic(dirErr)
+	}
+
+	return configPath
+}
+
+func readState(tokens *StravaToken) {
+	var (
+		configPath string = configPath()
+		origConfig []byte
+		err        error
+	)
+
+	origConfig, err = os.ReadFile(configPath)
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	} else if len(origConfig) == 0 {
+		origConfig = []byte("{}")
+		err = os.WriteFile(configPath, origConfig, 0600)
+		if err != nil {
+			log.Fatal(err)
+			panic(err)
+		}
+	}
+
+	if err := json.Unmarshal(origConfig, &tokens); err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	clientId := os.Getenv("SL_CLIENT_ID")
@@ -19,7 +83,8 @@ func main() {
 	authConfig.clientId = clientId
 	authConfig.clientSecret = clientSecret
 
-	// read locally the current Strava Tokens
+	var tokens StravaToken
+	readState(&tokens)
 
 	// fetch an access token (if necessary)
 
